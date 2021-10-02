da362ed8f0838a3db2c1d41e327249aec634e16d
Yunhua Li <yunhua.li@weride.ai>
2021-10-02 11:42:46 -0700

    Add java

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# cat shell/bashrc >> ~/.bashrc
-# cp shell/tmux.conf ~/.tmux.conf
 
 NVIM=$(which nvim)
 if [ "$NVIM" == "" ]; then
@@ -24,8 +22,29 @@ if [ ! -d ~/.config/nvim/ ]; then
 fi
 cp -a vim/* ~/.config/nvim/
 sudo pip install neovim
-cat ./shell/bashrc >> ~/.bashrc
+
+# cat ./shell/bashrc >> ~/.bashrc
+IFS=$'\n'
+all=$(cat shell/bashrc)
+for one in $all; do
+    out=$(cat ~/.bashrc | grep "$one")
+    if [ "$out" == "" ]; then
+        echo "append '$one' to ~/.bashrc"
+        echo $one >> ~/.bashrc
+    fi
+done
 cp ./shell/tmux.conf ~/
 
-echo "Run source ./shell/bashrc then open vim and run :PlugInstall"
-echo "then cd $HOME/.vim/plugged/YouCompleteMe; python3 ./install.py"
+nvim +'PlugInstall' +qall
+(cd $HOME/.vim/plugged/YouCompleteMe; python3 ./install.py)
+
+# for java
+# nvim +'CocInstall coc-java' +qall
+# sudo mkdir /usr/local/share/lombok
+# sudo wget https://projectlombok.org/downloads/lombok.jar -O /usr/local/share/lombok/lombok.jar
+# ( cd ~/.config/coc/extensions/coc-java-data/server; \
+#    wget https://download.eclipse.org/jdtls/milestones/0.57.0/jdt-language-server-0.57.0-202006172108.tar.gz \
+#    tar xvf jdt-language-server-0.57.0-202006172108.tar.gz )
+
+# echo "Run source ./shell/bashrc then open vim and run :PlugInstall"
+# echo "then cd $HOME/.vim/plugged/YouCompleteMe; python3 ./install.py"
